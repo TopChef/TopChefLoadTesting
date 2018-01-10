e270dd31996fd221acd55053b1865ae3159f46fc
Michal Kononenko <michalkononenko@gmail.com>
2018-01-10 01:38:40 -0500

    Added more load tests.

--- locustfile.py
+++ locustfile.py
@@ -1,23 +1,25 @@
 """
 Describes performance tests
 """
-from locust import HttpLocust, TaskSet, task
+from task_sets import ContactSite, CheckValidJSON
+from locust import HttpLocust
 
 
-class ContactSite(TaskSet):
+class WebsiteContact(HttpLocust):
     """
-    A simple test that simply contacts the site
-    """
-    @task(1)
-    def check_root(self):
-        self.client.get("/")
-
-
-class WebsiteUser(HttpLocust):
-    """
-    Fake user for the site
+    Locust that simply contacts the root user for the site
     """
     task_set = ContactSite
     min_wait = 100
     max_wait = 2000
+    weight = 1
 
+
+class JSONValidator(HttpLocust):
+    """
+    Locust that validates JSON against the server
+    """
+    task_set = CheckValidJSON
+    min_wait = 100
+    max_wait = 2000
+    weight = 1
